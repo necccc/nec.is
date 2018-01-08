df52cb26123830038356492c03be707326b73b23
nec <szabolcsit@hu.ibm.com>
2018-01-08 18:45:10 +0100

    fix page cards

--- scripts/social_tags.js
+++ scripts/social_tags.js
@@ -3,23 +3,39 @@
 
 hexo.extend.helper.register("social_tags", function () {
 
-    const image = ''
     let asset_dir = '/'
-    let title = ''
-    let description = ''
+    let title = 'nec.is'
+    let description = this.config.subtitle
     let url = this.config.url
+    let path = ''
+    let image = ''
 
     if (this.post) {
         asset_dir = this.post.asset_dir || '/'
-        title = this.post.title
-        description = this.post.summary
-        url += this.post.path
+        title = this.post.title || title
+        description = this.post.summary || description
+        path = this.post.path
     } else if (this.page) {
         asset_dir = this.page.asset_dir || '/'
-        title = this.page.title
-        description = this.page.summary
-        url += this.page.path
+        title = this.page.title || title
+        description = this.page.summary || description
+        path = this.page.path
     }
+
+    if (path.indexOf('index.html') > -1) {
+        path = '/' + path.replace('index.html', '')
+    }
+
+    url += path
+
+    if (this.post) {
+        image = url + 'social-card.png';
+    } else {
+        image = '/social-card.png';
+    }
+
+
+
 
     const twitter_user = '@' + this.config.twitter;
 
@@ -29,13 +45,13 @@ hexo.extend.helper.register("social_tags", function () {
         <meta name="twitter:creator" content="${twitter_user}">
         <meta name="twitter:title" content="${title}">
         <meta name="twitter:description" content="${description}">
-        <meta name="twitter:image" content="${url}social-card.png">
+        <meta name="twitter:image" content="${image}">
         <meta name="twitter:url" content="${url}">
 
         <meta property="og:type" content="website" />
         <meta property="og:title" content="${title}" />
         <meta property="og:description" content="${description}" />
-        <meta property="og:image" content="${url}social-card.png" />
+        <meta property="og:image" content="${image}" />
         <meta property="og:image:width" content="600" />
         <meta property="og:image:height" content="300" />
         <meta property="og:site_name" content="${twitter_user}" />
